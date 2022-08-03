39f24f8fa006da092731d6f5884ef19f000578f0
nasrinnoorahmad <nasrinnoorahmad@nasrins-mbp.verizon.net>
2022-08-03 17:38:34 -0400

    adding new method

--- src/team/g2/DemoG2.java
+++ src/team/g2/DemoG2.java
@@ -1,19 +1,23 @@
 package team.g2;
 
 public class DemoG2 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Test");
 
 		System.out.println("Nasrin is testing");
-		
+
 		funStuff();
 	}
-	
+
 	public static void funStuff() {
 		System.out.println("Prajita Testing......");
 	}
-	
-	//testing y seyar
+
+	// testing y seyar
+
+	public static void funStuff2() {
+		System.out.println("Prajita Testing......");
+	}
 
 }
