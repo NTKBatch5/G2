0b80db0fb15b12590067ff78df7442ed821bcad2
yourmajesty <yourmajesty@prajitas-mbp.fios-router.home>
2022-08-05 13:42:58 -0400

    change

--- src/team/g2/DemoG2.java
+++ src/team/g2/DemoG2.java
@@ -13,6 +13,9 @@ public class DemoG2 {
 
 		System.out.println("123");
 	}
+	public static void prajita() {
+		System.out.println("prajita..");
+	}
 
 	// Nasrin
 	// https://codingbat.com/prob/p165312
