3b3f19a0dddf08c6dd72991c5476f3ba6a4e8e6a
Seyarmansoori <seyarm2017@yahoo.com>
2022-08-04 21:49:54 -0400

    testing

--- src/team/g2/DemoG2.java
+++ src/team/g2/DemoG2.java
@@ -3,8 +3,6 @@ package team.g2;
 public class DemoG2 {
 
 	public static void main(String[] args) {
-		
-	     System.out.println("Seyar");
 		// Nasrin
 		DemoG2 obj = new DemoG2();
 		obj.doubleChar("The");
